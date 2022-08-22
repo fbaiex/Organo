fc7081b2b3bc112aaa20212a7c86ad55f00d7112
Fabio Aiex Alves <fabio.aiex@hotmail.com>
2022-08-22 16:06:15 -0300

    Primeiro Commit do Organo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Banner from './componentes/Banner';
+import Formulario from './componentes/Formulario';
+import { useState } from "react"
+import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Front-End II',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Front-End I',
+      corPrimaria: '#82CFFA',
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Modelagem de Dados',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2'
+    },
+  ]
+
+  // Cores primarias #DB6EBF #FFBA05 #FF8A29
+  //Cores secundarias #FAE9F5 #FFF5D9 #FFEEDF
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+
+      {times.map(time => <Time 
+        key={time.nome} 
+        nome={time.nome} 
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        /> )}
+      <Rodape />
     </div>
   );
 }
---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,47 @@
+import './Formulario.css'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
+import { useState } from "react"
+
+const Formulario = (props) => {
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
+    }
+
+
+    return (
+        <section className="formulario">
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador:</h2>
+                <CampoTexto obrigatorio={true} label="Nome do Professor:" placeholder="Digite seu nome" valor={nome} aoAlterado={valor => setNome(valor)}/>
+
+                <CampoTexto obrigatorio={true} label="Matéria:" placeholder="Digite o seu cargo" valor={cargo} aoAlterado={valor => setCargo(valor)}/>
+
+                <CampoTexto label="Foto do professor:" placeholder="Digite o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)}/>
+
+                <ListaSuspensa obrigatorio={true} label="Qual tipo de programador você é?" itens={props.times} valor={time} aoAlterado={valor => setTime(valor)}/>
+
+                <Botao texto="Criar card"/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
